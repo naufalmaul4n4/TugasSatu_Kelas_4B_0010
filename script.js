497951655a47b0537b855733c8a031aeb558d674
naufalmaulana <naufalmau3@gmail.com>
2026-05-07 21:55:38 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const inputValue = document.getElementById("inpuTask");
+const btnTambah = document.getElementById("btnTambahTodo");
+const daftarTugas = document.getElementById("listTugas");
+
+btnTambah.addEventListener("click", function(){
+
+    if(inputValue.value === ""){
+        alert("Input tidak boleh kosong");
+        return;
+    }
+
+    const listbaru = document.createElement("li");
+    const span = document.createElement("span")
+    span.innerHTML = inputValue.value;
+    
+    listbaru.appendChild(span);
+    daftarTugas.appendChild(listbaru)
+
+    inputValue.value = "";
+    inputValue.focus();
+
+
+
+})
